ac341815b94e39c2d9fa2a598d90418a4693a472
zineblouniri <zineblouniri@gmail.com>
2026-04-15 16:32:23 +0100

    Setup Express server, remove env file and update gitignore

---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
+
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
+
