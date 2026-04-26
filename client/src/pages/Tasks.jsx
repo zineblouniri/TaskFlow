fa275f224b07d80fafe7ae8bfdf3ea868ad296a5
zineblouniri <zineblouniri@gmail.com>
2026-04-26 16:46:46 +0100

    Add task creation feature with form and dynamic project handling

--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -4,6 +4,28 @@ import API from "../api/axios.js";
 const Tasks = () => {
   const { projectId } = useParams();
   const [tasks, setTasks] = React.useState([]);
+  const [newTask, setNewTask] = React.useState({
+    title: "",
+    description: "",
+    status: "todo",
+    priority: "medium",
+    deadline: ""
+  })
+
+  const handleChange = (e) => {
+    setNewTask({...newTask , [e.target.name]:e.target.value})
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        const res = await API.post('/tasks', {...newTask, project_id :projectId})
+        setTasks((prev) => [...prev, res.data])
+        setNewTask({title: "", description: "", status: "todo", priority: "medium", deadline: ""})
+    } catch (error) {
+        console.log("Error creating task:", error);
+    }
+  }
   const getTasks = async () => {
     try {
       const res = await API.get(`/tasks/${projectId}`);
@@ -17,6 +39,39 @@ const Tasks = () => {
   }, [projectId]);
   return (
     <div>
+        <form  onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Task title"
+            value={newTask.title}
+            onChange={handleChange}
+          />
+          <textarea 
+            name="description"
+            placeholder="Task description"
+            value={newTask.description}
+            onChange={handleChange}
+          />
+          <select name="status" id="status" value={newTask.status} onChange={handleChange}>
+            <option value="todo">To Do</option>
+            <option value="in_progress">In Progress</option>
+            <option value="done">Done</option>
+          </select>
+          <select name="priority" id="priority" value={newTask.priority} onChange={handleChange}>
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+          </select>
+          <input
+            type="date"
+            name="deadline"
+            value={newTask.deadline}
+            onChange={handleChange}
+          />
+          <button type="submit">Add Task</button>
+        </form>
       <h1>Tasks for project {projectId}</h1>
       {tasks.length === 0 ? (
         <p>No tasks found for this project</p>
