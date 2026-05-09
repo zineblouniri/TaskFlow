ed392a5f40ad0381a73fd286afcd6afa233f411b
zineblouniri <zineblouniri@gmail.com>
2026-05-09 17:46:38 +0100

    Prepare frontend for deployment using vercel

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const API = axios.create({
   //baseURL: "http://localhost:5000/api",
-  baseURL: "https://taskflow-humming-harborbird-7368.fly.dev/api"
+  //baseURL: "https://taskflow-humming-harborbird-7368.fly.dev/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 API.interceptors.request.use((req) => {
