1f50e288b2d83d0843c145ae2678101ce30cebb0
zineblouniri <zineblouniri@gmail.com>
2026-06-06 09:26:26 +0100

    fix: update API baseURL to ngrok

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const API = axios.create({
-  //baseURL: "http://localhost:5000/api",
+  baseURL:" https://motion-platform-haiku.ngrok-free.dev/api"
+  //baseURL: "http://localhost:8080/api",
   //baseURL: "https://taskflow-humming-harborbird-7368.fly.dev/api"
-  baseURL: import.meta.env.VITE_API_URL
+  //baseURL: import.meta.env.VITE_API_URL
 });
 
 API.interceptors.request.use((req) => {
